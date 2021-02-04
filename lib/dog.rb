e0bb5b55c44acac6c3472d2850ab86a75f40e9e2
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-04 17:37:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,14 @@
-# Your code goes here!
 class Dog
 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
+  attr_accessor :name
 
-  def name
-    @this_dogs_name
-  end
+def initialize
+  @name = name
+end
 
-  def bark=(dog_bark)
-    @this_dog_barks = dog_bark
-  end
+def bark
+  puts "woof!"
+end
 
-  def bark
-    @this_dog_barks
-    puts "woof!"
-  end
+
 end
